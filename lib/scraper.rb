1f7f1bff230e111b02d852eed0925e500314029c
Edsel Caprice <caprice@mail.usf.edu>
2018-07-14 15:21:35 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,7 +10,7 @@ class Scraper
      doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
    end
    def get_courses
-     self.get_page.css(".post")
+    get_page.css(".post")
    end
     # doc.css(".post").each do |post|
     #   course = Course.new
